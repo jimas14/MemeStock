ab8a463163f77cf4fb429236c24f094c8e7fb904
Allen Cheng <computer_allen@yahoo.com>
2016-09-17 15:55:33 -0400

    Angular first steps

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,21 @@
-angular
-  .module('thinkster', []);
+// angular.module('abc', []).controller('john', [$scope, $interpolateProvider,
+//
+//     function($scope, $interpolateProvider) {
+//     console.log('ff');
+//         $interpolateProvider.startSymbol('{[{');
+//       $interpolateProvider.endSymbol('}]}');
+//         $scope.a = 4;
+//
+//
+// }]);
+
+var app = angular.module('abc', []);
+
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[{');
+  $interpolateProvider.endSymbol('}]}');
+});
+
+app.controller('john', function($scope) {
+    $scope.a = 4;
+});

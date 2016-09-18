fffa4a2c582421caab2909155427123cbc17203d
Allen Cheng <computer_allen@yahoo.com>
2016-09-17 22:25:03 -0400

    d

--- static/javascripts/index-controller.js
+++ static/javascripts/index-controller.js
@@ -4,17 +4,17 @@ app.config(function ($stateProvider, $interpolateProvider) {
     $interpolateProvider.startSymbol('{[{');
     $interpolateProvider.endSymbol('}]}');
 
-    var loginState = {
-        name: 'login',
-        url: '/login',
-        template: '<button type="button" class="btn btn-primary" ng-model="s" uib-btn-checkbox btn-checkbox-true="1"> dsd</button>'
-        // templateUrl: '../templates/login.html',
-        // controller: 'LoginController'
-    };
-
-    $stateProvider.state(loginState);
+    // var loginState = {
+    //     name: 'login',
+    //     url: '/login',
+    //     template: '<button type="button" class="btn btn-primary" ng-model="s" uib-btn-checkbox btn-checkbox-true="1"> dsd</button>'
+    //     // templateUrl: '../templates/login.html',
+    //     // controller: 'LoginController'
+    // };
+    //
+    // $stateProvider.state(loginState);
 });
 
 app.controller('john', ['$scope', '$state', function ($scope, $state) {
-    $state.go('login');
+    // $state.go('login');
 }]);

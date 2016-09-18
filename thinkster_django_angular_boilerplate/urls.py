c89787df8a5a89f79df2ad28fd95706b9d44da1f
James Schleicher <jamesschleicher@Jamess-MacBook-Pro.local>
2016-09-18 02:24:44 -0400

    deleted unnecessary file

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -16,7 +16,7 @@ from thinkster_django_angular_boilerplate.views import indexView, investmentsVie
 urlpatterns = [
     url(r'^$', indexView, name='index'),
     url(r'^investments', investmentsView, name='investments'),
-    url(r'^transactions', transactionsView, name='transactions')
+    url(r'^transactions', transactionsView, name='transactions'),
 	url(r'^(?P<meme_name>\w{0,50})/$', views.get_results, name='get_results')
     #url(r'^login', loginView, name='login'),
     #url(r'^logout', logoutView, name='logout')

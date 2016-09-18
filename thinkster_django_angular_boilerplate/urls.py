7de2c5a029922e359a128cd0d3508e93fa7ffd20
James Schleicher <jamesschleicher@Jamess-MacBook-Pro.local>
2016-09-18 02:02:57 -0400

    added google trends api

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 
-from thinkster_django_angular_boilerplate.views import IndexView
+#from thinkster_django_angular_boilerplate.views import IndexView
+from thinkster_django_angular_boilerplate import views
 
 urlpatterns = patterns(
     '',
 
-    url('^.*$', IndexView.as_view(), name='index'),
+    #url(r'^.*$', IndexView.as_view(), name='index'),
+    url(r'^(?P<meme_name>\w{0,50})/$', views.get_results, name='get_results')
 )
--- thinkster_django_angular_boilerplate/views.py
+++ thinkster_django_angular_boilerplate/views.py
@@ -1,11 +1,43 @@
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
+from django.http import HttpResponse
+from pytrends.request import TrendReq
+import json
+
+def get_results(request, meme_name):
+	google_username = 'memestocks9000'
+	google_password = 'memesrule1'
+
+	pytrend = TrendReq(google_username, google_password, custom_useragent=None)
+
+	payload = {
+		'q' : 'harambe',
+		'date': 'today 1-d'
+	}
+
+	output = pytrend.trend(payload)
+
+	return HttpResponse(json.dumps(output), content_type="application/json")
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
 
-    @method_decorator(ensure_csrf_cookie)
-    def dispatch(self, *args, **kwargs):
-        return super(IndexView, self).dispatch(*args, **kwargs)
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+
+#     @method_decorator(ensure_csrf_cookie)
+#     def dispatch(self, *args, **kwargs):
+#         return super(IndexView, self).dispatch(*args, **kwargs)
+
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+
+#     @method_decorator(ensure_csrf_cookie)
+#     def dispatch(self, *args, **kwargs):
+#         return super(IndexView, self).dispatch(*args, **kwargs)
+
+
+
+# get request - get specific users' investments
+# get request - get all investments
+# post - to coinbase api
